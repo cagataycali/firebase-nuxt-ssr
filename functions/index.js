a7645ec917a2dc238ce5aaf37453dbc6b67f323e
cagataycali <cagataycali@icloud.com>
2019-04-27 22:02:23 +0300

    🎉  Init

---
+++ functions/index.js
@@ -0,0 +1,26 @@
+'use strict';const functions = require('firebase-functions');
+const { Nuxt } = require('nuxt');
+const express = require('express');
+
+const app = express();
+
+const config = {
+  dev: false,
+  buildDir: 'nuxt',
+  build: {
+    publicPath: '/' } };
+
+
+const nuxt = new Nuxt(config);
+
+function handleRequest(req, res) {
+  res.set('Cache-Control', 'public, max-age=150, s-maxage=300');
+  nuxt.renderRoute('/').then(result => {
+    console.log(result.html);
+    res.send(result.html);
+  }).catch(e => {console.log(e);res.send(e);});
+}
+
+app.get('**', handleRequest);
+
+exports.app = functions.https.onRequest(app);
